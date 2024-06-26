1209ee84a083480531a2a18be82b25687bc0d01f
ayush kumar <myself_ayush@proton.me>
2024-06-26 18:37:57 +0530

    z

---
+++ python/notes.py
@@ -0,0 +1,30 @@
+#  notes 
+# print(len(input()))
+# # this command made for we count word by giving input then they count how many word will we there
+
+#  a = input()
+#  this function take and give same output.
+
+# \n for change paragraph
+# print(" water " + input("what is wale"))  for output [ water + reply ]
+
+'''    this a multi line command you will make your notes without any interuption '''
+
+'''
+print(len("write anything that you will want to count))
+
+string - str 
+variable - change always
+float - decimal 10.78
+integer - int - negave positive number
+string - str = "write anything that will we a string " 
+
+string always written as same or you will space only
+
+
+
+
+
+
+
+'''
