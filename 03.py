29c69caed5bd614ac7666b684fd29c9a23250654
ayush <myself_ayush@outlook.com>
2024-06-26 18:08:46 +0530

    c add

--- 03.py
+++ 03.py
@@ -1,3 +1,7 @@
 print(len("water in the wave flow through the forest"))
 
 
+zx = (len(input(("now, we are resting on the surface"))))
+
+xc = str(zx)
+print(xc + " that's your number")
--- notes.py
+++ notes.py
@@ -13,6 +13,14 @@
 '''
 print(len("write anything that you will want to count))
 
+string - str 
+variable - change always
+float - decimal 10.78
+integer - int - negave positive number
+string - str = "write anything that will we a string " 
+
+string always written as same or you will space only
+
 
 
 
