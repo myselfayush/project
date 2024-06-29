617d9225207245a5cdfa5804aba9c2f5610fcc74
ayush kumar <myself_ayush@proton.me>
2024-06-29 09:22:18 +0530

    update

--- python/BMI.py
+++ python/BMI.py
@@ -18,4 +18,14 @@ bmi = w / (h * h)
 bmi_string = str(bmi)
 print ("you bmi index is " + bmi_string)
 
+
+if bmi_string > '17':
+    print("you are overweight")
+elif bmi_string > '24':
+    print("you are obese")
+elif bmi_string > 30:
+    print("you are clinically obese")
+elif bmi_string > 35:
+    print("you are morbidly obese")
+
 # now, i finished my code by understand and solving the problem
---
+++ python/elif multiple condition.py
@@ -0,0 +1,19 @@
+print (" welcome xyz company. age verification for company")
+
+
+age = int(input("enter your age? " ))
+
+
+
+if age < 18:
+      print("you are not eligible for this job")
+elif age > 18 < 22:
+    print("you are early start for this job")
+elif age > 25 < 30:
+    print("you are perfect for this job")
+elif age > 30 < 40:
+        print("you are experienced for this job")
+elif age > 23 < 24:
+            print("you are fresher for this job")
+else:
+ print("you are not eligible for this job")
